9dce00520d65a1f4d520989bd336df05578fcf5f
jisuanjiwanghu <425432986@qq.com>
2017-11-10 13:48:59 +0800

    新增选择功能，口令红包搞定

--- src/com/huwang/Main.java
+++ src/com/huwang/Main.java
@@ -2,6 +2,7 @@ package com.huwang;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,10 @@ public class Main {
         System.out.println("请输入抢红包的人数:");
         int n = scanner.nextInt();
         String[] name = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("请输入第" + (i + 1) + "个人的姓名");
+            name[i] = scanner.next();
+        }
         System.out.println("请输入红包金额:");
         double money = scanner.nextDouble();
         System.out.println("请输入红包个数:");
@@ -25,10 +30,7 @@ public class Main {
         switch (j) {
             case 1:
                 General_red_packets get = new General_red_packets(money, number);
-                for (int i = 0; i < n; i++) {
-                    System.out.println("请输入第" + (i + 1) + "个人的姓名");
-                    name[i] = scanner.next();
-                }
+
                 for (int i = 0; i < n; i++) {
                     get.get(name[i]);
                 }
@@ -36,16 +38,24 @@ public class Main {
             case 2:
                 Lucky_red_packets get1 = new Lucky_red_packets(money, number);
                 for (int i = 0; i < n; i++) {
-                    System.out.println("请输入第" + (i + 1) + "个人的姓名");
-                    name[i] = scanner.next();
-                }
-                for (int i = 0; i < n; i++) {
                     get1.get(name[i]);
                 }
                 if (n<number)
                     System.out.println((money- Lucky_red_packets.enter_money)+"元红包都没人要了？");
                 break;
             case 3:
+                Password_red_packets get2=new Password_red_packets(money,number);
+                System.out.println("请输入红包口令:");
+                String pass=scanner.next();
+                for (int i = 0; i < n; i++) {
+                    System.out.print(name[i]+"输入口令：");
+                    String password=scanner.next();
+                    if (Objects.equals(pass, password)) {
+                        get2.get(name[i],pass);
+                    }
+                }
+                if (n<number)
+                    System.out.println((money- Lucky_red_packets.enter_money)+"元红包都没人要了？");
                 break;
             default:
                 System.out.println("你皮？");
@@ -77,7 +87,7 @@ class General_red_packets {
 class Lucky_red_packets {
     private final int number;
     private final double money;
-    static int i = 5;
+    static int i = 0;
     static double min = 0.01;
     static double enter_money = 0;
     static String lucky_name;
@@ -87,10 +97,10 @@ class Lucky_red_packets {
         this.money = money;
         this.number = number;
     }
-
     void get(String name) {
         double rand = Math.random();
-        if (i > 1) {
+        i++;
+        if (i < number) {
             double rate = (money - enter_money - min * i) * rand + min;
             BigDecimal df = new BigDecimal(rate);
             rate = df.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
@@ -100,7 +110,7 @@ class Lucky_red_packets {
             }
             System.out.println(name + "拿了" + rate);
             enter_money += rate;
-        } else if (i == 1) {
+        } else if (i == number) {
             BigDecimal de = new BigDecimal(money - enter_money);
             enter_money = de.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
             if (enter_money > lucky_money) {
@@ -111,6 +121,44 @@ class Lucky_red_packets {
             System.out.println("运气王是" + lucky_name);
         } else
             System.out.println(name + "来晚了");
-        i--;
+    }
+}
+class Password_red_packets{
+    private final int number;
+    private final double money;
+    static int i = 0;
+    static double min = 0.01;
+    static double enter_money = 0;
+    static String lucky_name;
+    static double lucky_money = 0;
+
+    Password_red_packets(double money, int number) {
+        this.money = money;
+        this.number = number;
+    }
+    void get(String name,String password) {
+        i++;
+        double rand = Math.random();
+        if (i < number) {
+            double rate = (money - enter_money - min * i) * rand + min;
+            BigDecimal df = new BigDecimal(rate);
+            rate = df.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (rate > lucky_money) {
+                lucky_name = name;
+                lucky_money = rate;
+            }
+            System.out.println(name +"大喊一声"+password+ "拿了" + rate);
+            enter_money += rate;
+        } else if (i == number) {
+            BigDecimal de = new BigDecimal(money - enter_money);
+            enter_money = de.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            if (enter_money > lucky_money) {
+                lucky_name = name;
+                lucky_money = enter_money;
+            }
+            System.out.println(name +"大喊一声"+password+ "拿了" + enter_money);
+            System.out.println("运气王是" + lucky_name);
+        } else
+            System.out.println(name + "来晚了");
     }
 }
